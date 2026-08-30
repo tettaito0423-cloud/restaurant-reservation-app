2ce3909cdca802bfb77993b674940a26c1e501ef
Tetta Ito <tettaito0423@gmail.com>
2026-08-30 23:17:30 +0900

    Update reservation UI

--- app/calendar/dinner/dinnermanagement/DinnerManager.tsx
+++ app/calendar/dinner/dinnermanagement/DinnerManager.tsx
@@ -8,25 +8,33 @@ export default function DinnerManager({
     scheData:DinnerSlot[];
 }) {
     return (
-        <div>
-            <div className={styles.card}>
-                <div>
-                    <p>{scheData[0].startAt}</p>
-                    <p>1~2名</p>
-                    <p>3名</p>
-                    <p>4名~</p>
+        <div className={styles.page}>
+            <div className={styles.list}>
+                <input type="text" />
+            </div>
+            <div className={styles.info}>
+                <div className={styles.card}>
+                    <div className={styles.timeSlot}>
+                        <p>{scheData[0].startAt}</p>
+                        <div className={styles.limit}>
+                            <p>1~2名</p>
+                            <p>3名</p>
+                            <p>4名~</p>
+                        </div>
+                    </div>
+                    <div className={styles.timeSlot}>
+                        <p>{scheData[1].startAt}</p>
+                        <div className={styles.limit}>
+                            <p>1~2名</p>
+                            <p>3名</p>
+                            <p>4名~</p>
+                        </div>
+                    </div>
                 </div>
                 <div>
-                    <p>{scheData[1].startAt}</p>
-                    <p>1~2名</p>
-                    <p>3名</p>
-                    <p>4名~</p>
+                    <p>時間未定</p>
                 </div>
             </div>
-            <div>
-                <p>時間未定</p>
-            </div>
-
         </div>
     );
 }
